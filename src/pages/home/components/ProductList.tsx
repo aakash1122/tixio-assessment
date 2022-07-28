4fb37a32b38924a8ef6188bcfa4bcab54bc0333d
aakash1122 <aakaashhossain@gmail.com>
2022-07-28 12:44:53 +0600

    cleanup

--- src/pages/home/components/ProductList.tsx
+++ src/pages/home/components/ProductList.tsx
@@ -1,7 +1,7 @@
 import ProductCard from "../../../components/productCard";
 import { products } from "../../../data";
-type Props = {};
-const ProductList = (props: Props) => {
+
+const ProductList = () => {
   return (
     <div className="container grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 py-12">
       {products.map((product, i) => (
